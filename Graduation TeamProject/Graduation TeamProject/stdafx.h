e379bf512df9cbb10f53e2680ab5e25b43453117
Choi Hae Seong <2018180044@tukorea.ac.kr>
2022-09-21 23:52:00 +0900

    CreateBufferResource 함수 선언 및 정의
    버퍼를 생성하는 함수인 CreateBufferResource 함수를 선언하고 정의하였음

--- Graduation TeamProject/Graduation TeamProject/stdafx.h
+++ Graduation TeamProject/Graduation TeamProject/stdafx.h
@@ -44,3 +44,5 @@ using Microsoft::WRL::ComPtr;
 // 주 윈도우 클라이언트 영역의 크기를 표현하는 상수들입니다.
 #define FRAME_BUFFER_WIDTH 800
 #define FRAME_BUFFER_HEIGHT 600
+
+extern ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, void* pData, UINT nBytes, D3D12_HEAP_TYPE d3dHeapType = D3D12_HEAP_TYPE_UPLOAD, D3D12_RESOURCE_STATES d3dResourceStates = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, ID3D12Resource** ppd3dUploadBuffer = NULL);
