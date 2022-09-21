64556eda94812dd18e96b0204584c37b8f4a393a
Choi Hae Seong <2018180044@tukorea.ac.kr>
2022-09-21 23:56:03 +0900

    수정 날짜 추가 및 Mesh 클래스 함수들 정의
    수정 날짜를 추가하였고 Mesh 클래스 관련 함수들을 정의해주었음

--- Graduation TeamProject/Graduation TeamProject/Mesh.cpp
+++ Graduation TeamProject/Graduation TeamProject/Mesh.cpp
@@ -1,2 +1,169 @@
 #include "stdafx.h"
 #include "Mesh.h"
+
+//----------------------------------------------------------------------------
+// 22/09/21 Mesh 함수들 정의 - ChoiHS
+//----------------------------------------------------------------------------
+
+// Mesh 생성자
+CMesh::CMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, char* pstrFileName)
+{
+	if (pstrFileName) LoadMeshFromFile(pd3dDevice, pd3dCommandList, pstrFileName);
+}
+
+CMesh::~CMesh()
+{
+}
+
+void CMesh::ReleaseUploadBuffers()
+{
+	if (m_pd3dPositionUploadBuffer) m_pd3dPositionUploadBuffer.Reset();
+	if (m_pd3dNormalUploadBuffer) m_pd3dNormalUploadBuffer.Reset();
+	if (m_ppd3dIndexUploadBuffers.data())
+	{
+		m_ppd3dIndexUploadBuffers.clear();
+		std::vector<ComPtr<ID3D12Resource>>().swap(m_ppd3dIndexUploadBuffers);
+	}
+};
+
+void CMesh::OnPreRender(ID3D12GraphicsCommandList* pd3dCommandList)
+{
+	pd3dCommandList->IASetPrimitiveTopology(m_d3dPrimitiveTopology);
+	pd3dCommandList->IASetVertexBuffers(m_nSlot, m_nVertexBufferViews, m_pd3dVertexBufferViews.data());
+}
+
+void CMesh::Render(ID3D12GraphicsCommandList* pd3dCommandList, UINT nSubset)
+{
+	if (m_nSubsets > 0)
+	{
+		pd3dCommandList->IASetIndexBuffer(&m_pd3dIndexBufferViews[nSubset]);
+		pd3dCommandList->DrawIndexedInstanced(m_pnSubSetIndices[nSubset], 1, 0, 0, 0);
+	}
+	else
+	{
+		pd3dCommandList->DrawInstanced(m_nVertices, 1, m_nOffset, 0);
+	}
+}
+
+void CMesh::LoadMeshFromFile(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, char* pstrFileName)
+{
+#ifdef _WITH_TEXT_MESH
+	ifstream InFile(pstrFileName);
+
+	char pstrToken[64] = { '\0' };
+
+	for (; ; )
+	{
+		InFile >> pstrToken;
+		if (!InFile) break;
+
+		if (!strcmp(pstrToken, "<Vertices>:"))
+		{
+			InFile >> m_nVertices;
+			m_pxmf3Positions = new XMFLOAT3[m_nVertices];
+			for (UINT i = 0; i < m_nVertices; i++) InFile >> m_pxmf3Positions[i].x >> m_pxmf3Positions[i].y >> m_pxmf3Positions[i].z;
+		}
+		else if (!strcmp(pstrToken, "<Normals>:"))
+		{
+			InFile >> pstrToken;
+			m_pxmf3Normals = new XMFLOAT3[m_nVertices];
+			for (UINT i = 0; i < m_nVertices; i++) InFile >> m_pxmf3Normals[i].x >> m_pxmf3Normals[i].y >> m_pxmf3Normals[i].z;
+		}
+		else if (!strcmp(pstrToken, "<Indices>:"))
+		{
+			InFile >> m_nIndices;
+			m_pnIndices = new UINT[m_nIndices];
+			for (UINT i = 0; i < m_nIndices; i++) InFile >> m_pnIndices[i];
+		}
+	}
+#else
+	FILE* pFile = NULL;
+	::fopen_s(&pFile, pstrFileName, "rb");
+	::rewind(pFile);
+
+	char pstrToken[64] = { '\0' };
+
+	BYTE nStrLength = 0;
+	UINT nReads = 0;
+
+	while (!::feof(pFile))
+	{
+		nReads = (UINT)::fread(&nStrLength, sizeof(BYTE), 1, pFile);
+		if (nReads == 0) break;
+		nReads = (UINT)::fread(pstrToken, sizeof(char), nStrLength, pFile);
+		pstrToken[nStrLength] = '\0';
+
+		if (!strcmp(pstrToken, "<BoundingBox>:"))
+		{
+			nReads = (UINT)::fread(&m_xmBoundingBox.Center, sizeof(float), 3, pFile);
+			nReads = (UINT)::fread(&m_xmBoundingBox.Extents, sizeof(float), 3, pFile);
+		}
+		else if (!strcmp(pstrToken, "<Vertices>:"))
+		{
+			nReads = (UINT)::fread(&m_nVertices, sizeof(int), 1, pFile);
+			m_pxmf3Positions.resize(m_nVertices);
+			nReads = (UINT)::fread(m_pxmf3Positions.data(), sizeof(float), 3 * m_nVertices, pFile);
+		}
+		else if (!strcmp(pstrToken, "<Normals>:"))
+		{
+			nReads = (UINT)::fread(&m_nVertices, sizeof(int), 1, pFile);
+			m_pxmf3Normals.resize(m_nVertices);
+			nReads = (UINT)::fread(m_pxmf3Normals.data(), sizeof(float), 3 * m_nVertices, pFile);
+		}
+		else if (!strcmp(pstrToken, "<Indices>:"))
+		{
+			nReads = (UINT)::fread(&m_nIndices, sizeof(int), 1, pFile);
+			m_pnIndices.resize(m_nIndices);
+			nReads = (UINT)::fread(m_pnIndices.data(), sizeof(UINT), m_nIndices, pFile);
+		}
+		else if (!strcmp(pstrToken, "<SubMeshes>:"))
+		{
+			nReads = (UINT)::fread(&m_nSubsets, sizeof(int), 1, pFile);
+
+			m_pnSubSetIndices.resize(m_nSubsets);
+			m_pnSubSetStartIndices.resize(m_nSubsets);
+			m_ppnSubSetIndices.resize(m_nSubsets);
+
+			for (UINT i = 0; i < m_nSubsets; i++)
+			{
+				nReads = (UINT)::fread(&m_pnSubSetStartIndices[i], sizeof(UINT), 1, pFile);
+				nReads = (UINT)::fread(&m_pnSubSetIndices[i], sizeof(UINT), 1, pFile);
+				nReads = (UINT)::fread(&m_nIndices, sizeof(int), 1, pFile);
+				m_ppnSubSetIndices[i].resize(m_pnSubSetIndices[i]);
+				nReads = (UINT)::fread(m_ppnSubSetIndices[i].data(), sizeof(UINT), m_pnSubSetIndices[i], pFile);
+			}
+
+			break;
+		}
+	}
+
+	::fclose(pFile);
+#endif
+
+	m_pd3dPositionBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, m_pxmf3Positions.data(), sizeof(XMFLOAT3) * m_nVertices, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, &m_pd3dPositionUploadBuffer);
+	m_pd3dNormalBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, m_pxmf3Normals.data(), sizeof(XMFLOAT3) * m_nVertices, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, &m_pd3dNormalUploadBuffer);
+
+	m_nVertexBufferViews = 2;
+	m_pd3dVertexBufferViews.resize(m_nVertexBufferViews);
+
+	m_pd3dVertexBufferViews[0].BufferLocation = m_pd3dPositionBuffer->GetGPUVirtualAddress();
+	m_pd3dVertexBufferViews[0].StrideInBytes = sizeof(XMFLOAT3);
+	m_pd3dVertexBufferViews[0].SizeInBytes = sizeof(XMFLOAT3) * m_nVertices;
+
+	m_pd3dVertexBufferViews[1].BufferLocation = m_pd3dNormalBuffer->GetGPUVirtualAddress();
+	m_pd3dVertexBufferViews[1].StrideInBytes = sizeof(XMFLOAT3);
+	m_pd3dVertexBufferViews[1].SizeInBytes = sizeof(XMFLOAT3) * m_nVertices;
+
+	m_ppd3dIndexBuffers.resize(m_nSubsets);
+	m_ppd3dIndexUploadBuffers.resize(m_nSubsets);
+	m_pd3dIndexBufferViews.resize(m_nSubsets);
+
+	for (UINT i = 0; i < m_nSubsets; i++)
+	{
+		m_ppd3dIndexBuffers[i] = ::CreateBufferResource(pd3dDevice, pd3dCommandList, m_ppnSubSetIndices[i].data(), sizeof(UINT) * m_pnSubSetIndices[i], D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_INDEX_BUFFER, &m_ppd3dIndexUploadBuffers[i]);
+
+		m_pd3dIndexBufferViews[i].BufferLocation = m_ppd3dIndexBuffers[i]->GetGPUVirtualAddress();
+		m_pd3dIndexBufferViews[i].Format = DXGI_FORMAT_R32_UINT;
+		m_pd3dIndexBufferViews[i].SizeInBytes = sizeof(UINT) * m_pnSubSetIndices[i];
+	}
+}
