68f0941e9ef7e295fcafe4a72c0f2c454aa7ec3e
Choi Hae Seong <2018180044@tukorea.ac.kr>
2023-01-05 23:18:48 +0900

    Play with ImGui

--- Graduation TeamProject/Rampage/ImGui/ImGuiManager.cpp
+++ Graduation TeamProject/Rampage/ImGui/ImGuiManager.cpp
@@ -52,38 +52,58 @@ void CImGuiManager::OnPreRender()
     ImGui_ImplDX12_NewFrame();
     ImGui_ImplWin32_NewFrame();
     ImGui::NewFrame();
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+    //if (show_demo_window)
+    //    ImGui::ShowDemoWindow(&show_demo_window);
+    //// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+    //{
+    //    static float f = 0.0f;
+    //    static int counter = 0;
+
+    //    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+    //    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+    //    ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+    //    ImGui::Checkbox("Another Window", &show_another_window);
+    //    ImGui::Checkbox("My Window", &show_my_window);
+    //    
+    //    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+    //    ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+    //    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+    //        counter++;
+    //    ImGui::SameLine();
+    //    ImGui::Text("counter = %d", counter);
+
+    //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    //    ImGui::End();
+    //}
+
+    //// 3. Show another simple window.
+    //if (show_another_window)
+    //{
+    //    ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+    //    ImGui::Text("Hello from another window!");
+    //    if (ImGui::Button("Close Me"))
+    //        show_another_window = false;
+    //    ImGui::End();
+    //}
+
+    // Show my window.
     {
-        static float f = 0.0f;
-        static int counter = 0;
-
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 25, main_viewport->WorkPos.y + 25));
+        ImGui::SetNextWindowSize(ImVec2(1280, 680), ImGuiCond_FirstUseEver);
+        ImGui::Begin("My Window");   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text("Start My Window!");
+        ImGui::SetNextItemWidth(225.f);
+        ImGui::SetCursorPosX(855.f);
         ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
+        //ImGui::ColorEdit3("clear color", (float*)&clear_color, ImGuiColorEditFlags_NoInputs);
+        //ImGui::ColorEdit3("clear color", (float*)&clear_color, ImGuiColorEditFlags_NoLabel);
+        ImVec2 curpos = ImGui::GetCursorPos();
+        ImGui::SetCursorPosY(curpos.y - 25.f);
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window)
-    {
-        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-            show_another_window = false;
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         ImGui::End();
     }
 }
