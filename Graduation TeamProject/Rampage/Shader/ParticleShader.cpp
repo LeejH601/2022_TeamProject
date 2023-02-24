3d74f6bae32547184a5a58c46999f49c0c148990
Choi Hae Seong <2018180044@tukorea.ac.kr>
2023-02-24 23:03:10 +0900

    Render Particle

--- Graduation TeamProject/Rampage/Shader/ParticleShader.cpp
+++ Graduation TeamProject/Rampage/Shader/ParticleShader.cpp
@@ -33,19 +33,19 @@ void CParticleShader::CreateShader(ID3D12Device* pd3dDevice, ID3D12RootSignature
 D3D12_BLEND_DESC CParticleShader::CreateBlendState(int nPipelineState)
 {
 	D3D12_BLEND_DESC d3dBlendDesc;
-	::ZeroMemory(&d3dBlendDesc, sizeof(D3D12_BLEND_DESC));
-	d3dBlendDesc.AlphaToCoverageEnable = FALSE;
-	d3dBlendDesc.IndependentBlendEnable = FALSE;
-	d3dBlendDesc.RenderTarget[0].BlendEnable = TRUE;
-	d3dBlendDesc.RenderTarget[0].LogicOpEnable = FALSE;
-	d3dBlendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
-	d3dBlendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
-	d3dBlendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
-	d3dBlendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ZERO;
-	d3dBlendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
-	d3dBlendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
-	d3dBlendDesc.RenderTarget[0].LogicOp = D3D12_LOGIC_OP_NOOP;
-	d3dBlendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+    ::ZeroMemory(&d3dBlendDesc, sizeof(D3D12_BLEND_DESC));
+    d3dBlendDesc.AlphaToCoverageEnable = FALSE;
+    d3dBlendDesc.IndependentBlendEnable = FALSE;
+    d3dBlendDesc.RenderTarget[0].BlendEnable = TRUE;
+    d3dBlendDesc.RenderTarget[0].LogicOpEnable = FALSE;
+    d3dBlendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+    d3dBlendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+    d3dBlendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+    d3dBlendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+    d3dBlendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+    d3dBlendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+    d3dBlendDesc.RenderTarget[0].LogicOp = D3D12_LOGIC_OP_NOOP;
+    d3dBlendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
 
 	return(d3dBlendDesc);
 }
