8418213bf8fcfc547a4763ef66f00cec15911cb0
LeejH601 <77761502+LeejH601@users.noreply.github.com>
2023-03-10 19:07:19 +0900

    라이트 섀프트 기본 효과 적용
    라이트 새프트 효과가 어느정도 들어가는 것이 확인은 되나, 차폐로 인하여 음영이 져야하는 부분도 굉장히 밝아지는 문제가 있음.
    차폐 검사를 통해 이러한 부분들이 어둡게 나오도록 수정할 필요가 있으며, 라이트 새프트의 몇가지 상수들을 세부적으로 조정할 필요가 있음.

--- Graduation TeamProject/Rampage/Shader/PostProcessingPixelShader.hlsl
+++ Graduation TeamProject/Rampage/Shader/PostProcessingPixelShader.hlsl
@@ -1,7 +1,42 @@
 
+cbuffer cbCameraInfo : register(b1)
+{
+	matrix gmtxView : packoffset(c0);
+	matrix gmtxProjection : packoffset(c4);
+	matrix gmtxInverseProjection : packoffset(c8);
+	matrix gmtxInverseView : packoffset(c12);
+	float3 gf3CameraPosition : packoffset(c16);
+};
 
 Texture2D gtxMultiRenderTargetTextures[7] : register(t35);
 SamplerState gSamplerState : register(s0);
+
+#include "Light.hlsl"
+
+#define NUM_SAMPLES 64
+#define Density 0.6f
+//#define Weight 0.2f
+#define Decay 0.7f
+#define Exposure 1.0f
+
+float4 LightShaft(float2 uv, float2 ScreenLightPos, float Weight)
+{
+	float2 DeltaUV = (uv - ScreenLightPos.xy);
+	DeltaUV *= 1.0f / NUM_SAMPLES * Density;
+
+	float3 color = gtxMultiRenderTargetTextures[0].Sample(gSamplerState, uv);
+	float illuminationDecay = 1.0f;
+
+	for (int i = 0; i < NUM_SAMPLES; i++) {
+		uv -= DeltaUV;
+		float3 sampleColor = gtxMultiRenderTargetTextures[0].Sample(gSamplerState, uv);
+		sampleColor *= illuminationDecay * Weight;
+		color += sampleColor;
+		illuminationDecay *= Decay;
+	}
+
+	return float4(color * Exposure, 1);
+}
 
 struct VS_SCREEN_RECT_TEXTURED_OUTPUT
 {
@@ -10,10 +45,35 @@ struct VS_SCREEN_RECT_TEXTURED_OUTPUT
 	float3 viewSpaceDir : TEXCOORD1;
 };
 
+#define radius 0.7f
+
 float4 PS_PostProcessing(VS_SCREEN_RECT_TEXTURED_OUTPUT input) : SV_Target
 {
-	float4 cColor = gtxMultiRenderTargetTextures[0].Sample(gSamplerState, input.uv);
+	float4 lightPosInViewSpace = mul(float4(gLights[0].m_vPosition, 1.0), gmtxView);
+	float4 lightPosInClipSpace = mul(lightPosInViewSpace, gmtxProjection);
+	float3 lightPosInNDC = lightPosInClipSpace.xyz / lightPosInClipSpace.w;
+	float2 lightPosInScreenSpace = float2(lightPosInNDC.x * 0.5 + 0.5, -lightPosInNDC.y * 0.5 + 0.5);
+
+	float result = pow((input.uv.x - lightPosInScreenSpace.x), 2) + pow((input.uv.y - lightPosInScreenSpace.y), 2);
+
+
+	//float4 cColor = gtxMultiRenderTargetTextures[0].Sample(gSamplerState, input.uv);
 	//float4 cColor = float4(1.0f, 0.0f, 0.0f, 1.0f);
+	float weight = 0.4f;
+	float radiusPow = pow(radius, 2);
+	float4 cColor;
+
+	if (lightPosInNDC.z > 1.0f) {
+		cColor = gtxMultiRenderTargetTextures[0].Sample(gSamplerState, input.uv);
+		return cColor;
+	}
+
+	if (result < radiusPow) {
+		weight *= (1.0f - (result / radiusPow));
+		cColor = LightShaft(input.uv, lightPosInScreenSpace, weight);
+	}
+	else
+		cColor = gtxMultiRenderTargetTextures[0].Sample(gSamplerState, input.uv);
 
 	return cColor;
 }
