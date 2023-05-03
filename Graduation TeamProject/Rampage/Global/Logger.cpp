0dc2751be6f8fd42332f1d79d2a4db52b6d47f1c
LeejH601 <77761502+LeejH601@users.noreply.github.com>
2023-05-03 20:56:59 +0900

    Logging 전처리기

--- Graduation TeamProject/Rampage/Global/Logger.cpp
+++ Graduation TeamProject/Rampage/Global/Logger.cpp
@@ -33,11 +33,14 @@ bool CLogger::ClearLogs()
 
 void CLogger::Log(std::string& message, LOG_LEVEL)
 {
+#ifdef LOGGING
 	out.write(message.c_str(), message.length());
+#endif // LOGGING
 }
 
 void CLogger::LogCollision(CGameObject* Actor, CGameObject* Target, std::string& message, LOG_LEVEL)
 {
+#ifdef LOGGING
 	std::string LogMessage;
 	const std::time_t t_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock().now());
 
@@ -48,4 +51,5 @@ void CLogger::LogCollision(CGameObject* Actor, CGameObject* Target, std::string&
 	LogMessage = "Collision - Actor { " + std::string(Actor->m_pstrFrameName) + " } : Target { " +
 		std::string(Target->m_pstrFrameName) + " } : Message { " + message + " } : Time { " + timestr + " }\n";
 	out.write(LogMessage.c_str(), LogMessage.length());
+#endif // LOGGING
 }
